7bb97c91208291f805a2879f5455c92e5afeb130
OceansBlue <OceansBlueCode@gmail.com>
2018-12-23 23:28:35 -0600

    Code

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,21 @@
+# Practice to learn 
+
+# Print
+
+print ("hello Texas")
+
+print (5 + 1000)
+print (1000 - 200)
+print ("Hi Nancy")
+
+greetings = 'Welcome'
+name = "Nancy"
+print (greetings + ' ' + name)
+
+##
+
+greetings = 'Welcome'
+name = input ('Please enter your name: ' )
+print (greetings + ' ' + name)
+
+
