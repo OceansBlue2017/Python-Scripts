4fa822d6e752294565e136caaae81ad663055e4b
OceansBlue <OceansBlueCode@gmail.com>
2018-12-24 22:03:30 -0600

    more code 2018-12-24 10:04pm

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -18,4 +18,20 @@ greetings = 'Welcome'
 name = input ('Please enter your name: ' )
 print (greetings + ' ' + name)
 
+# Split a string into two lines by using the \n
+string1 = 'This String has been split \n by a backslash n'
+print (string1)
+
+# Tab a string by using the \t  it causes a tab space
+string2 = 'This String has been tabbed  \t by a backslash t'
+print (string2)
+
+# Different ways to print a quote inside the string text
+print ('Adam says "Hello from inside the qoute" ')  #  this is a method 
+print ('Adam says \'Hello from inside the qoute\' ')  #  this is another method 
+print ("Adam says \"Hello from inside the qoute\" ")  #  this is another method 
+
+
+
+
 
