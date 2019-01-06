1fcaf4e1afc7944202f99e68db35aa7aa3087521
OceansBlue <OceansBlueCode@gmail.com>
2019-01-05 18:32:59 -0600

    Changed name to PythonPractice

---
+++ PythonApplication1/PythonApplication1/PythonPractice.py
@@ -0,0 +1,37 @@
+# Practice to learn 
+
+# Print
+
+print ("hello Texas")
+
+print (5 + 1000)
+print (1000 - 200)
+print ("Hi Nancy")
+
+greetings = 'Welcome'
+name = "Nancy"
+print (greetings + ' ' + name)
+
+##
+
+greetings = 'Welcome'
+name = input ('Please enter your name: ' )
+print (greetings + ' ' + name)
+
+# Split a string into two lines by using the \n
+string1 = 'This String has been split \n by a backslash n'
+print (string1)
+
+# Tab a string by using the \t  it causes a tab space
+string2 = 'This String has been tabbed  \t by a backslash t'
+print (string2)
+
+# Different ways to print a quote inside the string text
+print ('Adam says "Hello from inside the qoute" ')  #  this is a method 
+print ('Adam says \'Hello from inside the qoute\' ')  #  this is another method 
+print ("Adam says \"Hello from inside the qoute\" ")  #  this is another method 
+
+
+
+
+
